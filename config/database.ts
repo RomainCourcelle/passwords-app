ebd9f1646a88240998d5ac12b20b789d6b5c3a1b
Romain <romain@Mac.fritz.box>
2025-10-01 18:12:46 -1000

    Configure for production deployment 2 & database

--- config/database.ts
+++ config/database.ts
@@ -2,10 +2,24 @@ import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'mysql',
+  connection: env.get('DB_CONNECTION', 'mysql'),
   connections: {
     mysql: {
       client: 'mysql2',
+      connection: {
+        host: env.get('DB_HOST'),
+        port: env.get('DB_PORT'),
+        user: env.get('DB_USER'),
+        password: env.get('DB_PASSWORD'),
+        database: env.get('DB_DATABASE'),
+      },
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+    },
+    pg: {
+      client: 'pg',
       connection: {
         host: env.get('DB_HOST'),
         port: env.get('DB_PORT'),
